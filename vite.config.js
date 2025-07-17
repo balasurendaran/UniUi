80a54ba23ad2ce625c15cc72081922609923caa4
Bala Surendaran .R <balaraja8182@gmail.com>
2025-07-17 21:57:00 +0530

    vite-plugin-compression added

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      algorithm: "gzip",
+      ext: ".gz",
+      deleteOriginFile: false, // set to true if you want to delete original files
+    }),
+  ],
   resolve: {
     alias: {
       components: path.resolve(__dirname, "src/components"),
